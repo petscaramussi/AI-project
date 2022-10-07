14cce76d5434439422b3cab64d85f443be4abc8b
PetScaramussi <peterson.silva6@fatec.sp.gov.br>
2022-10-06 22:08:22 -0300

    fix bugs

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,17 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/template/header/header.component';
-import { ConfirmComponent } from './components/confirm/confirm.component';
-
-
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { ChatComponent } from './components/chat/chat.component';
 import { FormsModule } from '@angular/forms';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { ConfirmComponent } from './components/confirm/confirm.component';
+import { MatButtonModule } from '@angular/material/button'
 
 @NgModule({
   declarations: [
@@ -26,7 +24,8 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
     BrowserAnimationsModule,
     MatToolbarModule,
     FormsModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
